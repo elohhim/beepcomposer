65a1f9ea1438e5d66d01031cd413cc095eef7f6c
elohhim <elohhim@gmail.com>
2019-02-12 15:28:56 +0100

    Setup changes to enable PyPI publishing.

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 """Setup beepcomposer module."""
+import setuptools
 
-from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='beepcomposer',
-      version="0.1.0",
-      description="Small module to play melodies with OS beep functionality.",
-      url="http://github.com/elohhim/beepcomposer",
-      author="Jan Kumor",
-      author_email='elohhim at mail service provided by creators of popular '
-                   'search engine starting with letter g',
-      license='MIT',
-      packages=['beepcomposer'])
+
+setuptools.setup(
+    name='beepcomposer',
+    version="0.1.0",
+    author="Jan Kumor",
+    author_email='elohhim@gmail.com',
+    license='MIT',
+    description="Small module to play melodies with OS beep functionality.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/elohhim/beepcomposer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows"
+    ],
+)
